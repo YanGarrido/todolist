639427beb60491804ac01259077d15cd11168d9d
Yan <yangarridodev@gmail.com>
2025-09-15 19:07:21 -0300

    feat: Criação do UserModel e UserController

---
+++ src/main/java/br/com/yangarrido/todolist/User/UserModel.java
@@ -0,0 +1,11 @@
+package br.com.yangarrido.todolist.User;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+
+  private String username;
+  private String user;
+  private String password;
+}
